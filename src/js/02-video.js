a062b74b0139d622137c2237a155390bc25a8660
Taras Bondar <strelezzzz@gmail.com>
2022-05-18 00:01:25 +0300

    8.2 need throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,10 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
 
 player.on('play', function () {
   console.log('played the video!');
@@ -10,3 +13,32 @@ player.on('play', function () {
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+// You can listen for events in the player by attaching a callback using .on():
+player.on('timeupdate', function (data) {
+  console.log(data.seconds);
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+});
+// data is an object containing properties specific to that event
+// {
+//   duration: 61.857;
+//   percent: 0.049;
+//   seconds: 3.034;
+// }
+// setCurrentTime(seconds: number): Promise<number, (RangeError|Error)>
+player
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+  .then(function (seconds) {
+    console.log(seconds);
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
