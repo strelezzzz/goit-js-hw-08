35485c154e8cd33e8e872f55b683d9f9fdbe4d9d
Taras Bondar <strelezzzz@gmail.com>
2022-05-19 00:00:10 +0300

    start task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
 
+// Відстежуй на формі подію input, і щоразу записуй у локальне
+//  сховище об'єкт з полями email і message,
+// у яких зберігай поточні значення полів форми.
+// Нехай ключем для сховища буде рядок "feedback-form-state".
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    email: document.querySelector(".feedback-form input"),
+    massage: document.querySelector(".feedback-form textarea"),
+};
+
+refs.email.addEventListener('submit', onEmailInput);
+refs.massage.addEventListener('input', onTextAreaInput);
+
+
+
+
