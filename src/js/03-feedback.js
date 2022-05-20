0cfc079f8a2efd91be16d6ab3ec336651fe07755
Taras Bondar <strelezzzz@gmail.com>
2022-05-21 00:58:23 +0300

    update 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,12 +7,30 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const refs = {
     email: document.querySelector(".feedback-form input"),
-    massage: document.querySelector(".feedback-form textarea"),
+    message: document.querySelector(".feedback-form textarea"),
 };
 
-refs.email.addEventListener('submit', onEmailInput);
-refs.massage.addEventListener('input', onTextAreaInput);
+refs.email.addEventListener('submit', onFormSubmit);
+refs.message.addEventListener('input', onTextAreaInput);
 
 
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log("Send Form");
+    evt.currentTarget.reset;
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onTextAreaInput(evt) {
+    const message = evt.currentTarget.value;
+    localStorage.setItem(STORAGE_KEY, message);
+};
+
+function populateOnTextArea(evt) {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        refs.textarea.value = savedMessage;
+    }
+}
 
 
