aeb0116cf0200103b8d3963097353ad069aec187
Taras Bondar <strelezzzz@gmail.com>
2022-05-22 00:27:55 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,5 @@
 
+import throttle from "lodash.throttle";
 // Відстежуй на формі подію input, і щоразу записуй у локальне
 //  сховище об'єкт з полями email і message,
 // у яких зберігай поточні значення полів форми.
@@ -10,27 +11,35 @@ const refs = {
     message: document.querySelector(".feedback-form textarea"),
 };
 
-refs.email.addEventListener('submit', onFormSubmit);
-refs.message.addEventListener('input', onTextAreaInput);
+refs.email.addEventListener('input', throttle(onEmail,500));
+refs.message.addEventListener('input', throttle(onTextAreaInput, 500));
 
+populateOnTextArea()
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+    evt.currentTarget.reset();
     console.log("Send Form");
-    evt.currentTarget.reset;
+   
     localStorage.removeItem(STORAGE_KEY);
+     
 };
 
 function onTextAreaInput(evt) {
-    const message = evt.currentTarget.value;
+    const message = evt.target.value;
     localStorage.setItem(STORAGE_KEY, message);
 };
 
-function populateOnTextArea(evt) {
+function populateOnTextArea() {
     const savedMessage = localStorage.getItem(STORAGE_KEY);
     if (savedMessage) {
-        refs.textarea.value = savedMessage;
+        refs.message.value = savedMessage;
     }
 }
 
-
+function onEmail() {
+    
+}
